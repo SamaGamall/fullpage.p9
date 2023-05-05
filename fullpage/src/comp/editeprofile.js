134c5ca96ea4abe91c5713931789a03a186ccc05
SamaGamall <samagamal024@gmail.com>
2023-05-05 05:45:48 +0300

    essage

--- fullpage/src/comp/editeprofile.js
+++ fullpage/src/comp/editeprofile.js
@@ -1,150 +1,162 @@
 import React from 'react';
 import './editeprofile.css';
 const Editeprofile = () => {
-  return (
-    <div className="container1-3 rounded bg-white mt-5 mb-5">
-    <div className="row">
-      <div className="col-md-3 border-right">
-        <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-          <img
-            className="rounded-circle mt-5"
-            width="150px"
-            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-          />
-          <span className="font-weight-bold">Edogaru</span>
-          <span className="text-black-50">edogaru@mail.com.my</span>
-          <span> </span>
-        </div>
-      </div>
-      <div className="col-md-5 border-right">
-        <div className="p-3 py-5">
-          <div className="d-flex justify-content-between align-items-center mb-3">
-            <h4 className="text-right">Profile Settings</h4>
-          </div>
-          <div className="row mt-2">
-            <div className="col-md-6">
-              <label className="labels">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="first name"
-                defaultValue=""
-              />
+  return (<div className="container1-2"style={{   paddingtop: '20rem' }}>
+  <div className="row gutters">
+    <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+      <div className="card3 h-100">
+        <div className="card-body">
+          <div className="account-settings">
+            <div className="user-profile">
+              <div className="user-avatar">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                  alt="Maxwell Admin"
+                />
+              </div>
+              <h5 className="user-name">Yuki Hayashi</h5>
+              <h6 className="user-email">yuki@Maxwell.com</h6>
             </div>
-            <div className="col-md-6">
-              <label className="labels">Surname</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue=""
-                placeholder="surname"
-              />
+            <div className="about">
+              <h5>About</h5>
+              <p>
+                I'm Yuki. Full Stack Designer I enjoy creating user-centric,
+                delightful and human experiences.
+              </p>
             </div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-md-12">
-              <label className="labels">Mobile Number</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter phone number"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Address Line 1</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter address line 1"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Address Line 2</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter address line 2"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Postcode</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter address line 2"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">State</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter address line 2"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Area</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter address line 2"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Email ID</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="enter email id"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-12">
-              <label className="labels">Education</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="education"
-                defaultValue=""
-              />
-            </div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-md-6">
-              <label className="labels">Country</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="country"
-                defaultValue=""
-              />
-            </div>
-            <div className="col-md-6">
-              <label className="labels">State/Region</label>
-              <input
-                type="text"
-                className="form-control"
-                defaultValue=""
-                placeholder="state"
-              />
-            </div>
-          </div>
-          <div className="mt-5 text-center">
-            <button className="btn6 btn-primary profile-button" type="button" >
-              Save Profile
-            </button>
           </div>
         </div>
       </div>
-    
+    </div>
+    <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+      <div className="card3 h-100">
+        <div className="card-body">
+          <div className="row gutters">
+            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <h6 className="mb-2 text-primary">Personal Details</h6>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="fullName"
+                  placeholder="Enter full name"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="eMail">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="eMail"
+                  placeholder="Enter email ID"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phone"
+                  placeholder="Enter phone number"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="website">Website URL</label>
+                <input
+                  type="url"
+                  className="form-control"
+                  id="website"
+                  placeholder="Website url"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row gutters">
+            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <h6 className="mt-3 mb-2 text-primary">Address</h6>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="Street">Street</label>
+                <input
+                  type="name"
+                  className="form-control"
+                  id="Street"
+                  placeholder="Enter Street"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="ciTy">City</label>
+                <input
+                  type="name"
+                  className="form-control"
+                  id="ciTy"
+                  placeholder="Enter City"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="sTate">State</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="sTate"
+                  placeholder="Enter State"
+                />
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+              <div className="form-group">
+                <label htmlFor="zIp">Zip Code</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="zIp"
+                  placeholder="Zip Code"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row gutters">
+            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <div className="text-right">
+                <button
+                  type="button"
+                  id="submit"
+                  name="submit"
+                  className="btn6 btn-secondary"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  id="submit"
+                  name="submit"
+                  className="btn6 btn-primary"
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+</div>
+
   
   );
 }
--- fullpage/src/pages/profile.js
+++ fullpage/src/pages/profile.js
@@ -1,8 +1,6 @@
-import Header from '../comp/header';
-import Footer from '../comp/Footer';
-import MainComponant from '../comp/MainComponant';
+
 import Userprofile from '../comp/Userprofile';
-import Icon from '@mui/material/Icon';
+
 const Profile = () => {
   return (
     <>
